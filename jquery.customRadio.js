8c2fb436c3ace3a2331a2bb4cdedb8e52b62c107
Jeremy Fields <jeremy.fields@viget.com>
2013-07-31 09:24:54 -0600

    Adding plugin

---
+++ jquery.customRadio.js
@@ -0,0 +1,69 @@
+(function($) {
+
+	$.customRadio = function(el, options) {
+		// To avoid scope issues, use 'base' instead of 'this'
+		// to reference this class from internal events and functions.
+		var base = this;
+
+		// Access to jQuery and DOM versions of element
+		base.$el = $(el);
+		base.el = el;
+
+		// Add a reverse reference to the DOM object
+		base.$el.data("customRadio", base);
+
+		// init
+		base.init = function() {
+			base.options = $.extend({},$.customRadio.defaultOptions, options);
+
+			base.vars();
+
+			//
+			if (base.$el.is(':checked')) {
+				base.$parent.addClass(base.options.checked);
+			}
+
+			base.$el.on({
+				focus: function() {
+					base.$parent.addClass(base.options.focused);
+				},
+				blur: function() {
+					base.$parent.removeClass(base.options.focused);
+				},
+				click: function() {
+					if (base.$el.is(':checked')) {
+						if (base.$el.attr('type') === 'radio') {
+							base.$siblings.closest('label').removeClass(base.options.checked);
+						}
+						base.$parent.addClass(base.options.checked);
+					} else {
+						base.$parent.removeClass(base.options.checked);
+					}
+				}
+			});
+		};
+
+		// variables
+		base.vars = function() {
+			base.$el.off('focus.replace blur.replace click.replace');
+			base.$parent = base.$el.closest('label').addClass(base.options.init);
+			base.$siblings = $('input[name="' + base.$el.attr('name') + '"]').not(base.$el);
+		};
+
+		// Run initializer
+		base.init();
+	};
+
+	$.customRadio.defaultOptions = {
+		init: "control-replace-js",
+		checked: "control-checked",
+		focused: "control-focused"
+	};
+
+	$.fn.customRadio = function(options) {
+		return this.each(function() {
+			(new $.customRadio(this, options));
+		});
+	};
+
+})(jQuery);
